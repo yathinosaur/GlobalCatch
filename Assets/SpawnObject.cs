0937f2eab6871aac05af4317bc28921ea371bfde
Yathindra Shivakumar <32390768+yathinosaur@users.noreply.github.com>
2020-08-23 03:29:55 -0400

    Initial commit

---
+++ Assets/SpawnObject.cs
@@ -0,0 +1,28 @@
+﻿
+using UnityEngine;
+
+public class SpawnObject : MonoBehaviour
+{
+    [SerializeField] private GameObject[] projectiles;
+    [SerializeField] private Vector3 startPos;
+    [SerializeField] private Camera camera;
+
+    private GameObject currentProjectile;
+    private int index = 0;
+x
+    public void LaunchProjectile()
+    {
+        if (projectiles[index].GetComponent<ProjectileMotion>() == null)
+            projectiles[index].AddComponent<ProjectileMotion>();
+        Vector3 spawnPos = camera.transform.position + startPos.z * camera.transform.forward + startPos.y * camera.transform.up;
+        currentProjectile = Instantiate(projectiles[index], spawnPos, new Quaternion());
+        currentProjectile.GetComponent<ProjectileMotion>().isKinematic = true;
+        ShiftIndex();
+    }
+    private void ShiftIndex()
+    {
+        ++index;
+        if (index >= projectiles.Length)
+            index = 0;
+    }
+}
